4ab0022586e332c58446e17792ea726f915a3447
chae1108 <k_1108@hotmail.com>
2016-06-13 17:42:50 +0900

    add stuff

--- js/index.js
+++ js/index.js
@@ -49,24 +49,6 @@ angular
         value: '새가슴 추천4'
     }];
 
-    $scope.padData = [{
-        id: "img/flat.png",
-        title: '업음',
-        value: '업음 추천1'
-      },{
-        id: "img/cone.png",
-        title: '얇은 패드',
-        value: '얇은 패드 추천2'
-      },{
-        id: "img/hemi.png",
-        title: '컵 아래부분의 반정도 볼륨이 있는 중간 패드',
-        value: '컵 아래부분의 반정도 볼륨이 있는 중간 패드 추천3'
-    },{
-        id: "img/drooping.png",
-        title: '컵 전체를 거의다 볼륨으로 채우는 두꺼운 패드',
-        value: '컵 전체를 거의다 볼륨으로 채우는 두꺼운 패드 추천4'
-    }];
-    
 
     $scope.radioData = [
       { label: '1', value: 1 },
