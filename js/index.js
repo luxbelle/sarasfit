04cfcc12ef2d343482a968165d56cb52206eb1e6
chae1108 <k_1108@hotmail.com>
2016-06-13 18:06:25 +0900

    add stuff

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ angular
   .controller('AppCtrl', function($scope) {
     
     $scope.submit =function() {
-      $scope.display = $scope.data.group1 + $scope.data.group2;
+      $scope.display = $scope.data.group1 + $scope.data.group2  + $scope.data.group3;
   };
 
     $scope.data = {
       group1 : '추천브라1',
-      group2 : '추천브라2'
+      group2 : '추천브라2',
       group3 : '추천브라3',
     };
 
@@ -49,6 +49,23 @@ angular
         value: '새가슴 추천4'
     }];
 
+    $scope.padData = [{
+        id: "img/flat.png",
+        title: '없음',
+        value: '없음 추천1'
+      },{
+        id: "img/cone.png",
+        title: '얇은 패드',
+        value: '얇은 패드 추천2'
+      },{
+        id: "img/hemi.png",
+        title: '컵 아래부분의 반정도 볼륨이 있는 중간 패드',
+        value: '컵 아래부분의 반정도 볼륨이 있는 중간 패드 추천3'
+    },{
+        id: "img/drooping.png",
+        title: '컵 전체를 거의다 볼륨으로 채우는 두꺼운 패드',
+        value: '컵 전체를 거의다 볼륨으로 채우는 두꺼운 패드 추천4'
+    }];
 
     $scope.radioData = [
       { label: '1', value: 1 },
